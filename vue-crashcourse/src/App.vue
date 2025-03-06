950a046e40004e86ddd30fbc6cdf41c056c715d0
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2025-03-06 16:03:20 +0100

    added more components

--- vue-crashcourse/src/App.vue
+++ vue-crashcourse/src/App.vue
@@ -1,5 +1,12 @@
-<script setup></script>
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+</script>
 
 <template>
-  <h1 class="text-2xl">Vue Jobs</h1>
+  <Navbar />
+  <Hero />
+  <!-- <Hero title="Test Title" subtitle="Test Subtitle" /> -->
+  <HomeCards />
 </template>
