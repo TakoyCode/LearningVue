696efc1b640173f2a8b679f6e2e56d5d0b035209
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2025-03-19 20:45:28 +0100

    Finished crash course

--- vue-crashcourse/src/views/EditJobView.vue
+++ vue-crashcourse/src/views/EditJobView.vue
@@ -1,8 +1,14 @@
 <script setup>
 import router from '@/router';
 import axios from 'axios';
-import { reactive } from 'vue';
+import { useRoute } from 'vue-router';
+import { reactive, onMounted } from 'vue';
 import { useToast } from 'vue-toastification';
+
+const route = useRoute();
+const jobId = route.params.id;
+
+const toast = useToast();
 
 const form = reactive({
   type: 'Full-Time',
@@ -18,10 +24,13 @@ const form = reactive({
   },
 });
 
-const toast = useToast();
+const state = reactive({
+  job: {},
+  isLoading: true,
+});
 
 const handleSubmit = async () => {
-  const newJob = {
+  const updatedJob = {
     title: form.title,
     type: form.type,
     location: form.location,
@@ -35,14 +44,42 @@ const handleSubmit = async () => {
     },
   };
   try {
-    const response = await axios.post('/api/jobs', newJob);
-    toast.success('Job Added Successfully');
+    const response = await axios.put(`/api/jobs/${jobId}`, updatedJob);
+    toast.success('Job Updated Successfully');
     router.push(`/jobs/${response.data.id}`);
   } catch (error) {
-    console.error('Error fetching job', error);
-    toast.error('Job Was Not Added');
+    console.error('Error Updating Job', error);
+    toast.error('Job Was Not Updated');
   }
 };
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`/api/jobs/${jobId}`);
+    state.job = response.data;
+
+    // Updating Form
+    form.title = state.job.title;
+    form.type = state.job.type;
+    form.location = state.job.location;
+    form.description = state.job.description;
+    form.salary = state.job.salary;
+    form.company.name = state.job.company.name;
+    form.company.description = state.job.company.description;
+    form.company.contactEmail = state.job.company.contactEmail;
+    form.company.contactPhone = state.job.company.contactPhone;
+
+    // Should be a shorter way to write it, maybe more prone to breakage if you change anything in the job object
+    // Object.assign(form, {
+    //   ...state.job,
+    //   company: { ...state.job.company },
+    // });
+  } catch (error) {
+    console.error('Error fetching job', error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
@@ -52,7 +89,7 @@ const handleSubmit = async () => {
         class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
       >
         <form @submit.prevent="handleSubmit">
-          <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
+          <h2 class="text-3xl text-center font-semibold mb-6">Update Job</h2>
 
           <div class="mb-4">
             <label for="type" class="block text-gray-700 font-bold mb-2"
@@ -207,7 +244,7 @@ const handleSubmit = async () => {
               class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Add Job
+              Update Job
             </button>
           </div>
         </form>
--- vue-crashcourse/src/views/JobView.vue
+++ vue-crashcourse/src/views/JobView.vue
@@ -1,17 +1,40 @@
 <script setup>
 import BackButton from '@/components/BackButton.vue';
 import { reactive, onMounted } from 'vue';
-import { useRoute, RouterLink } from 'vue-router';
+import { useRoute, RouterLink, useRouter } from 'vue-router';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
+import { useToast } from 'vue-toastification';
+
+// Imports router instance directly, so works globally.
+import router from '@/router';
+
+// Only works inside Vue components, since it uses Vue Composition API to get the router instance
+// const router = useRouter();
 
 const route = useRoute();
+const toast = useToast();
+
 const jobId = route.params.id;
 
 const state = reactive({
   job: {},
   isLoading: true,
 });
+
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm('Are you sure you want to delete this job?');
+    if (confirm) {
+      const response = await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job Deleted Successfully');
+      router.push(`/jobs`);
+    }
+  } catch (error) {
+    console.error('Error Deleting Job', error);
+    toast.error('Job Was Not Deleted');
+  }
+};
 
 onMounted(async () => {
   try {
@@ -39,9 +62,7 @@ onMounted(async () => {
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
-              <i
-                class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-              ></i>
+              <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
@@ -99,6 +120,7 @@ onMounted(async () => {
               >Edit Job</RouterLink
             >
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
